ca50bb8d8a74277a400272a198477f7d24e3718e
Geoff Coutts <geoff.coutts@gmail.com>
2018-03-06 23:26:54 +0000

    Created first table migration for milestones table

---
+++ migrations/20180306214028_create_milestones.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.hasTable('milestones').then(function(exists) {
+      if (!exists) {
+        console.log('creating table')
+        knex.schema.createTable('milestones', function (table) {
+          console.log('running');
+          table.increments('id').primary();
+          table.string('description');
+          table.date('date_achieved');
+
+
+        })
+        .then(console.log('hi'))
+      }
+
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTableIfExists('milestones')
+  ])
+};
